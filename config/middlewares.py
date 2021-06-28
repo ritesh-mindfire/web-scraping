256d77c5de17008b1c7a35e9796cc788d2fcb921
Ritesh Goyal <ritesh.g@mindfiresolutions.com>
2021-06-28 15:53:39 +0530

    Update for simple middleware

---
+++ config/middlewares.py
@@ -0,0 +1,25 @@
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
+def simple_middleware(get_response):
+    # One-time configuration and initialization.
+
+    def middleware(request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+        
+        print("IP Address is {}".format(get_client_ip(request)))
+        response = get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
+
+    return middleware
--- config/settings.py
+++ config/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'config.middlewares.simple_middleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
